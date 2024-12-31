12bdcd5f8ceaaab5e10b19ca1148d634308dd9e6
yinxl <yinxl111@gmail.com>
2024-12-31 21:34:24 +0800

    Step1 Step2

--- tutorial/Step2/tutorial.cxx
+++ tutorial/Step2/tutorial.cxx
@@ -5,9 +5,14 @@
 
 #include "TutorialConfig.h"
 
-int main(int argc, char* argv[])
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
+
+int main(int argc, char *argv[])
 {
-  if (argc < 2) {
+  if (argc < 2)
+  {
     // report version
     std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
               << Tutorial_VERSION_MINOR << std::endl;
@@ -18,9 +23,17 @@ int main(int argc, char* argv[])
   // convert input to double
   const double inputValue = std::stod(argv[1]);
 
+// 根据参数，决定使用什么函数计算平方
+#ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+  std::cout << "if USE_MYMATH" << std::endl;
+#else
   // calculate square root
   const double outputValue = sqrt(inputValue);
+  std::cout << "else USE_MYMATH" << std::endl;
+#endif
+
   std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
+            << std::endl; 
   return 0;
 }
